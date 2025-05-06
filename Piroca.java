ac5a8ed448a9a9bdb4a1a63c981b3bceabed21a9
LuisPintodaFigueira <luissales1802@gmail.com>
2025-05-06 18:17:00 -0300

    Update Piroca.java
    PROJETO FINALIZADO

--- Piroca.java
+++ Piroca.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
-public class piroca {
+public class Piroca {
 
     public static Scanner tecladoScanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        double baseMaior, baseMenor, altura, area;
+
+        imprimir("Digite a base maior do trapézio: ");
+        baseMaior = lerValorReal();
+
+        imprimir("Digite a base menor do trapézio: ");
+        baseMenor = lerValorReal();
+
+        imprimir("Digite a altura do trapézio: ");
+        altura = lerValorReal();
+
+        area = areaTrap(baseMaior, baseMenor, altura);
+        imprimir("A área do trapézio é: " + area);
+    }
     public static void imprimir(String saida) {
         System.out.println(saida);
     }
